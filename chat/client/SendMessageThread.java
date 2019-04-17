e10b42e56053e42ce2ba62b42d6e71db53551f8e
Lily <32252211+caryophyllis@users.noreply.github.com>
2019-04-17 16:00:19 +0800

    Add files via upload

---
+++ chat/client/SendMessageThread.java
@@ -0,0 +1,35 @@
+package com.shengsiyuan.chat.client;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+import com.shengsiyuan.chat.util.CharacterUtil;
+
+
+public class SendMessageThread extends Thread
+{
+    private String text;
+    public SendMessageThread(String text)
+    {
+        this.text = text;       
+    }
+    
+    public void run()
+    {
+        try
+        {
+            DatagramSocket datagramSocket = new DatagramSocket();
+            int[] ports = CharacterUtil.string2Array(CharacterUtil.SERVER_PORT);
+            int port = ports[1];   
+            InetAddress address = InetAddress.getByName(CharacterUtil.SERVER_HOST);    
+            DatagramPacket datagramPacket = new DatagramPacket(text.getBytes(),text.length(),address,port);
+            datagramSocket.send(datagramPacket);
+            datagramSocket.close();
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+}
